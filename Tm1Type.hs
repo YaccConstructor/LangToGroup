ef795a3af429d5d24f4e8fdecba8d54ecff37b01
MaxVortman <maka.abort@gmail.com>
2018-10-05 15:04:28 +0300

    type -> newtype

--- Tm1Type.hs
+++ Tm1Type.hs
@@ -3,31 +3,31 @@ module Tm1Type where
 import Data.Set (Set)
 import qualified Data.Set as Set
 -- letter
-type Letter = Char
+newtype Letter = Letter Char
 -- input, tape alphabets
-type InputAlphabet = Set Letter
-type TapeAlphabet = Set Letter
+newtype InputAlphabet = InputAlphabet (Set Letter)
+newtype TapeAlphabet = TapeAlphabet (Set Letter)
 -- state of TM1
-type State = Char
+newtype State = State Char
 -- TM1 set states
-type States = Set State
+newtype States = States (Set State)
 -- k - vector of tapes states
-type MultiTapeStates = [States]
+newtype MultiTapeStates = MultiTapeStates [States]
 -- k - vector of start states
-type StartStates = [State]
+newtype StartStates = StartStates [State]
 -- k - vector of end states
-type AccessStates = [State]
+newtype AccessStates = AccessStates [State]
 -- right, left, none shift after end of command
 data Shift = R | L | None
 -- command of TM1
-type Command = (Letter, State, Letter) -> (Letter, State, Letter, Shift) 
+newtype Command = Command ((Letter, State, Letter) -> (Letter, State, Letter, Shift))
 -- commands
-type Commands = Set Command
+newtype Commands = Commands (Set Command)
 -- leftmost square on every tape
 leftBoundingLetter = 'α'
 -- rightmost square on every tape
 rightBoundingLetter = 'ω'
 -- configuration of tape
-type Configuration = ([Letter], State, [Letter])
+newtype Configuration = Configuration ([Letter], State, [Letter])
 -- TM1
-type TM1 = (InputAlphabet, TapeAlphabet, MultiTapeStates, Commands, StartStates, AccessStates)
+newtype TM1 = TM1 (InputAlphabet, TapeAlphabet, MultiTapeStates, Commands, StartStates, AccessStates)
