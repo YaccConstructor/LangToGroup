99a3a2283f635fa9d1b483b40e39e2f3bbd1de30
tursunova <st056641@student.spbu.ru>
2018-10-05 17:02:06 +0500

    Relation type fixed

--- GRType.hs
+++ GRType.hs
@@ -4,12 +4,9 @@ import Data.String
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-type Transition = (Set Char) -> (Set Char)
-type Auxiliary = (Set Char) -> (Set Char)
-type Hub = (Set Char) -> (Set Char)
-
 type A = Set Char
-data Relation = Transition | Auxiliary | Hub
+--type Relation = (A, A)
+type Relation = ((A, A), (A, A))
 
 data Relations = Set Relation
 
