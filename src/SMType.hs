3205517defc0bdabb4c0cde425eef970069581b5
tursunova <st056641@student.spbu.ru>
2018-11-01 18:36:12 +0500

    newtype Rule added

--- src/SMType.hs
+++ src/SMType.hs
@@ -14,4 +14,7 @@ newtype Q' = Q' (Set Char) deriving Show
 newtype Y = Y (Vector Y') deriving Show
 newtype Q = Q (Vector Q') deriving Show
 
-newtype SM =  SM (N, Y, Q) deriving Show
+newtype Word = Word (Set Char) deriving Show 
+newtype Rule = Rule (Word, Word) deriving Show
+
+newtype SM =  SM (N, Y, Q, Rule) deriving Show
