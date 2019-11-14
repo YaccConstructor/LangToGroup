6072abc0f890a69ff0b59fd9f56e1ce3b1cd2155
MaxVortman <vortmanmax@gmail.com>
2019-11-14 18:27:39 +0300

    fix start cfg

--- coq/TM.v
+++ coq/TM.v
@@ -24,58 +24,53 @@ Definition states {S : Type} : Type := list S.
 Inductive triple {S : Type} : Type := 
   | Triple (l : letter) (s : S) (r : letter).
 
-Notation "( l , s , r )" := (triple l s r).
+Notation "( l , s , r )" := (Triple l s r).
 
 Inductive command {S : Type} : Type :=
   | Command (t1 t2 : @triple S).
 
-Notation "t1 -> t2" := (command t1 t2).
+Notation "t1 -> t2" := (Command t1 t2).
+
+Notation "L / x" := (Letter x).
 
 Check @Letter nat 1.
 Check Letter 1.
-Check triple Empty 2 Empty.
-Check (triple (Letter 2) 2 (Letter 3)).
+Check (Triple (Letter 2) 2 (Letter 3)).
 Check (Empty, 2, Empty).
 Check ((Letter 1), 2, (Letter 3)).
 Check (Letter 1, 2, Letter 3) -> (Letter 2, 2, Letter 5).
+Check (L/1, 2, L/3) -> (L/2, 2, L/5).
 
 Inductive TM {A S} : Type :=
   | tm  (inp : @alphabet A)
         (tp : list (@alphabet A))
         (tpst : list (@states S))
-        (cmds : list (list (@command A S)))
+        (cmds : list (list (@command S)))
         (st : @states S)
         (fn : @states S).
 
-Inductive config {A S} : Type :=
-  | Config (l : list A) (s : S) (r : list A).
+Inductive config {S} : Type :=
+  | Config (l : list letter) (s : S) (r : list letter).
 
-Definition tm_configuration {A S} := list (@config A S).
+Definition tm_configuration {S} := list (@config S).
 
-Fixpoint make_config {A S}
-    (left : list (list A)) (statess : list S) (right : list (list A)) : tm_configuration :=
-      match left, statess, right with
-      | [], [], [] => []
-      | lh :: lt, sh :: st, rh :: rt => Config lh sh rh :: make_config lt st rt
-      | [], sh :: st, [] => Config [] sh [] :: make_config [] st []
-      | lh :: lt, sh :: st, [] => Config lh sh [] :: make_config lt st []
-      | [], sh :: st, rh :: rt => Config [] sh rh :: make_config [] st rt
-      | _, _, _ => []
-      end.
+Search fold_left.
+Compute fold_left (fun x y => x ++ y) [[0]; [3]] [5]. (** [5; 0; 3] **)
 
-Example e4 : make_config [[1]] [2] [[3]] = [Config [1] 2 [3]].
+Fixpoint make_start_config {S A}
+    (inp : list A) (sts : list S) : tm_configuration :=
+    match sts with
+    | [] => []
+    | sh :: st =>
+      let letter_inp := (LeftBounding :: map (fun i => L/i) inp) ++ [RightBounding] in
+      (Config letter_inp sh [RightBounding]) :: map (fun s => Config [LeftBounding] s [RightBounding]) st
+    end.
+
+Example e4 : make_start_config [1; 2; 3] [0] 
+  = [Config [LeftBounding; L/1; L/2; L/3; RightBounding] 0 [RightBounding]].
 Proof. reflexivity. Qed.
 
-Example e5 : make_config [[1]; [2]] [3] [[4]] = [Config [1] 3 [4]].
-Proof. reflexivity. Qed.
-
-Example e6 : make_config [[1]; [2]] [3; 4] [[5]] = [Config [1] 3 [5]; Config [2] 4 []].
-Proof. reflexivity. Qed.
-
-Definition start_config {A S} (input : list A) (st : @states S) : tm_configuration :=
-  make_config [input] st [].
-
-Fixpoint is_access {A S} (cfgs : @tm_configuration A S) (ast : @states S) :=
+Fixpoint is_access {S} (cfgs : @tm_configuration S) (ast : @states S) :=
   match cfgs, ast with
   | [], [] => True
   | (Config l s r) :: tc, ha :: ta => s = ha /\ is_access tc ta
@@ -84,10 +79,9 @@ Fixpoint is_access {A S} (cfgs : @tm_configuration A S) (ast : @states S) :=
 
 (*
 s1 == s && 
-(r1 == emptySymbol && l1 == rightBoundingLetter && r2 /= emptySymbol && l2 == rightBoundingLetter && l == [rightBoundingLetter] ||
-r /= [leftBoundingLetter] && r1 == (last r) && l1 == rightBoundingLetter && r2 == emptySymbol && l2 == rightBoundingLetter && l == [rightBoundingLetter] ||
-(last r) == r1 && (head l) == l1 ||
-r1 == emptySymbol && r2 == emptySymbol && l1 == rightBoundingLetter && l2 == l1) -> checkCommandTapeToTape t2 t1
+(r1 == emptySymbol && l1 == rightBoundingLetter && l2 == l1 && l == [] ||
+r1 == (last r emptySymbol) && l1 == rightBoundingLetter && r2 == emptySymbol && l2 == l1 && l == [] ||
+(last r) == r1 && (hd emptySymbol l) == l1 && r2 <> leftBoundingLetter && l2 <> rightBoundingLetter) -> checkCommandTapeToTape t2 t1
 *)
 
 Fixpoint get_applicable_cmds {A S} (cfgs : @tm_configuration A S) (cmds : list (list (@command A S))) 
@@ -95,7 +89,7 @@ Fixpoint get_applicable_cmds {A S} (cfgs : @tm_configuration A S) (cmds : list (
     let check cmd :=
       match cfgs, cmd with
       | [], [] => []
-      | (Config l1 s1 r1) :: tcfg, (Command l2 s2 r2) :: tcmd => s1 = s2 /\ 
+      | (Config l s r) :: tcfg, ((l1, s1, r1) -> (l2, s2, r2)) :: tcmd => s1 = s /\ (l1 = )
 
 Fixpoint interpret_tm {A} (tm : TM) (inp : list A) : bool :=
   match tm with 
