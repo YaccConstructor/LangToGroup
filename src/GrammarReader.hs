422315b6b513350a7dc0f746a50adb721232a994
katevi <catherine.vinnik>
2020-10-13 17:34:50 +0300

    Added parsing of start nonterminal

--- src/GrammarReader.hs
+++ src/GrammarReader.hs
@@ -5,7 +5,22 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Text (Text)
 import Data.Void
+import Data.Functor
+
+import GrammarType
 
 type Parser = Parsec Void Text
 
-main = do parseTest (satisfy (== 'a') :: Parser Char) "b"
+
+pStartSymbol :: Parser String
+pStartSymbol = (++) <$> (some upperChar) <*> (many lowerChar)
+
+pGrammar :: Parser StartSymbol
+pGrammar = do
+	startSymbol <- pStartSymbol
+	void (char ':')
+	return (Nonterminal startSymbol)
+
+--temporary added deriving show in Nonterminal in GrammarType module
+main = do parseTest (pGrammar <* eof) "Sba:"
+--main = do runParser(pGrammar <* eof) "" "Sba:"
