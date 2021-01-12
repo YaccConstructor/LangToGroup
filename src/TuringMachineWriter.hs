c8fa2c2719fd24a29d0c0a8809f217662911cd83
Glebanister <glebmar2001@gmail.com>
2021-01-13 01:30:21 +0300

    Implement show for TMS

--- src/TuringMachineWriter.hs
+++ src/TuringMachineWriter.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE FlexibleInstances #-}
-
 -- |This module provides functionality for presenting the Turing machine 'TM' in a text format appropriate to this service: https://turingmachinesimulator.com/
-module TuringMachineWriter (toTMSimulator) where
+-- Below this format will be called Tms.
+module TuringMachineWriter (showAsTms) where
 
 import GHC.Unicode (isAlphaNum)
 import Data.List.Utils (replace)
@@ -9,8 +8,70 @@ import Data.List (intercalate)
 
 import TMType
 
-class ShowTMSimulator a where
-    toTMSimulator :: a -> String
+showAsTms :: TM -> String
+showAsTms m = "TMS placeholder"
+
+-- |Type of Tms tape cell.
+--
+-- 'Any' is any type of cell.
+--
+-- 'Label' is labeled type of cell.
+data TmsTapeSquare = Any | Label Char
+
+tmssq2chars :: [Char] -> TmsTapeSquare -> [Char]
+tmssq2chars alphabet Any       = alphabet
+tmssq2chars alphabet (Label c) = pure c
+
+-- |Type of Tms tape head movement
+data TmsTapeHeadMovement = MoveLeft | Stay | MoveRight
+
+instance Show TmsTapeHeadMovement where
+    show MoveLeft  = "<"
+    show Stay      = "-"
+    show MoveRight = ">"
+
+-- |Type of Tms command for one tape.
+newtype TmsSingleTapeCommand = TmsSingleTapeCommand ((State, TmsTapeSquare), (State, TmsTapeSquare, TmsTapeHeadMovement))
+
+-- |Type of Tms command for entire Turing machine.
+newtype TmsCommand = TmsCommand [TmsSingleTapeCommand]
+
+-- |Type of Tms format.
+-- Tms (name, initState, acceptStates, commands, tapeAlphabets).
+newtype Tms = Tms (String, State, [State], [TmsCommand], [[Char]])
+
+instance Show Tms where
+    show
+        (Tms
+            (name,
+            (State init),
+            acStates,
+            commands,
+            tapeAlphabets)
+        ) = "name: " ++ name ++ "\n" ++
+        "init: " ++ filterStateName init ++ "\n" ++
+        "accept: " ++ intercalate ", " (map (\(State s) -> filterStateName s) acStates) ++ "\n\n" ++
+        intercalate "\n\n" (map showTmsCommand commands)
+        where
+            showTmsCommand :: TmsCommand -> String
+            showTmsCommand (TmsCommand tapeCommands) = intercalate "\n" $ map showSingleCmd $ combine $ map extCommand (zip tapeAlphabets tapeCommands)
+            extCommand :: ([Char], TmsSingleTapeCommand) -> [((State, Char), (State, Char, TmsTapeHeadMovement))]
+            extCommand (alph, (TmsSingleTapeCommand ((iniSt, iniSq), (folSt, folSq, mv)))) =
+                [((iniSt, iniChar), (folSt, folChar, mv)) | iniChar <- tmssq2chars alph iniSq,
+                                                              folChar <- tmssq2chars alph folSq]
+            combine :: Foldable t => t [a] -> [[a]]
+            combine = foldl (\combs cur -> flip (:) <$> combs <*> cur) [[]]
+            showSingleCmd :: [((State, Char), (State, Char, TmsTapeHeadMovement))] -> String
+            showSingleCmd cmds = intercalate ", " (iniStateName : iniSquares) ++
+                                 intercalate ", " (folStateName : folSquares ++ moves)
+                where
+                    iniStateName = mergeMultipleTapeStates $ map (\((ini, _), (_, _, _)) -> ini) cmds
+                    folStateName = mergeMultipleTapeStates $ map (\((_, _), (fol, _, _)) -> fol) cmds
+                    iniSquares = map (\((_, iniSq), (_, _, _)) -> return iniSq) cmds
+                    folSquares = map (\((_, _), (_, folSq, _)) -> return folSq) cmds
+                    moves = map (\((_, _), (_, _, mv)) -> show mv) cmds
+
+-- Section of helper functions.
 
 filterStateName :: String -> String
 filterStateName = replace "^" "v" . filter (\c -> isAlphaNum c || elem c ['_', '^'])
@@ -19,21 +80,3 @@ mergeMultipleTapeStates :: [State] -> String
 mergeMultipleTapeStates = ("Q__" ++ ) . intercalate "__" . map filterStateName . enum
     where
         enum ss = fmap (\(num, (State s)) -> show num ++ "__" ++ s) (zip [0 ..] ss)
-
-instance ShowTMSimulator [TapeCommand] where
-    toTMSimulator cmds = "command placeholder"
-
-instance ShowTMSimulator TM where
-    toTMSimulator
-        (TM
-            (inputAlphabet,
-            tapeAlphabets, 
-            MultiTapeStates multiTapeStates, 
-            Commands commands, 
-            StartStates startStates, 
-            AccessStates accessStates)
-        ) =
-        "name: TM\n" ++
-        "init: " ++ mergeMultipleTapeStates startStates ++ "\n" ++
-        "accept: " ++ mergeMultipleTapeStates accessStates ++ "\n\n" ++
-        concatMap ((++ "\n\n") . toTMSimulator) commands
