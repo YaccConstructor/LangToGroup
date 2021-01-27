af8a3341757b15d1324a5da4d7887c4bd00d6f7a
katevi <catherine.vinnik>
2021-01-27 14:58:51 +0300

    Started add implementation of helper functions

--- src/Boolean2TM.hs
+++ src/Boolean2TM.hs
@@ -2,6 +2,15 @@ module Boolean2TM where
 
 import GrammarType
 import TMTypes
+import Data.Char (chr, ord)
+
+newtype ConjunctionPair = ConjunctionPair (Integer, Nonterminal, Nonterminal)
+
+-- firstly generating descriptional states (name of states describes, which activities TM does in this state), 
+-- after convert it to state Q_index from TMTypes module via bijective transformation
+-- c |-> chr $ c + (ord 'a' - 1)
+-- (transformation is bijective, since will use only chars 'A'..'Z','a'..'z', '1'..'9', '-' with disjoint prefixes for debugging names)
+newtype DebuggingState = DState String
 
 {--boolean2tm :: Grammar -> TuringMachine
 boolean2tm
@@ -10,12 +19,10 @@ boolean2tm
         setOfTerminals,
         setOfRelations,
         Nonterminal startSymbol)) = do --}
---
-newtype ConjunctionPair = ConjunctionPair (Integer, Nonterminal, Nonterminal)
-
+ 
 -- naming for blocks of TM?
-generateQFindNewSubstitution :: Grammar -> Quadruples
-
+{-generateQFindNewSubstitution :: Grammar -> Quadruples
+  
 generateQRefineConjunctionDetails :: Grammar -> Quadruples
 
 generateQFoldConjCallNextConjFromSameRule :: Grammar -> Quadruples
@@ -30,14 +37,18 @@ generateQCreateNewSubstitution :: Grammar -> Quadruples
 
 generateQFigureWordOrSymbol :: Grammar -> Quadruples
 
-generateQGetResultForSymbol :: Grammar -> Quadruples
+generateQGetResultForSymbol :: Grammar -> Quadruples --}
 
 -- helper functions
-calculateMaxNumberOfRulesForNonterminal :: Grammar -> Integer
+convertDebuggingStateToState :: DebuggingState -> State
+convertDebuggingStateToState (DState string) = Q number where
+    number = read (concatMap (show . ord) string) :: Int
+
+{--calculateMaxNumberOfRulesForNonterminal :: Grammar -> Integer
 
 calculateNextConjunctionInSameRule :: ConjunctionPair -> ConjunctionPair
 
-calculateNextConjunctionInNextRule :: ConjunctionPair -> ConjunctionPair
+calculateNextConjunctionInNextRule :: ConjunctionPair -> ConjunctionPair--} 
 
 
 
