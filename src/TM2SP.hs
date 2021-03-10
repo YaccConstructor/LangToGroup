19bab6b99732ff678673ed55c22a37bdd2c755a8
Kuzivanov Sergey <rep78398@gmail.com>
2021-03-10 08:43:44 +0200

    Update TM -> SP converting

--- src/TM2SP.hs
+++ src/TM2SP.hs
@@ -5,7 +5,21 @@ import TMReader
 import SPTypes
 import SPGens
 import qualified Set
+import qualified Data.Set as RealSet
 import qualified Data.Map.Lazy as Map
+import Control.Monad (guard)
+
+setsOfSymbols :: TMReader [(State, [Symbol])]
+setsOfSymbols = do
+    TM qs' <- getT
+    m <- getM
+    let qs = Map.toList qs'
+        qsL = filter ((== L) . fst . snd) qs
+        allSymbols = RealSet.fromList $ S <$> [0..m]
+        q1s1s = Map.fromList $ map (\(q, s) -> (q, RealSet.singleton s)) $ map fst qsL
+        q2s = Map.fromSet (const allSymbols) $ RealSet.fromList $ map (snd . snd) qsL
+        qss = Map.unionWith RealSet.union q1s1s q2s
+    return $ map (\(q, ss) -> (q, RealSet.toList ss)) $ Map.toList $ qss
 
 (===) :: [TMReader Generator] -> [TMReader Generator] -> TMReader Relation
 w1 === w2 = Equals <$> sequence w1 <*> sequence w2
@@ -16,6 +30,7 @@ relations = do
     m <- getM
     TM quadruplesMap <- getT
     let quadruples = Map.toList quadruplesMap
+    sos <- setsOfSymbols
     fmap Set.fromList $ sequence $
         ( do
             quadruple <- quadruples
@@ -50,9 +65,21 @@ relations = do
                         else []
                     )
         ) ++
-        [[q_ alpha, s_ beta]  === [s_ beta, p_ alpha] | alpha <- [0..n], beta <- [0..m]] ++
-        [[q_ alpha, h_0]      === [h_0, p_ alpha] | alpha <- [0..n]] ++
-        [[q_ alpha, h_1]      === [h_1, p_ alpha] | alpha <- [0..n]] ++
+        [
+            [q_ alpha, s_ beta] === [s_ beta, p_ alpha]
+          | (q, ss) <- sos,
+            s <- ss,
+            let Q alpha = q,
+            let S beta = s
+        ] ++
+        ( do
+            (q, ss) <- sos
+            s <- ss
+            guard (s == S 0)
+            let Q alpha = q
+            h <- [h_0, h_1]
+            return $ [q_ alpha, h] === [h, p_ alpha]
+        ) ++
         [[q_ 0, h_0]          === [h_0, q_ 0]] ++
         [[q_ 0, s_ beta]      === [q_ 0]      | beta <- [0..m]] ++
         [[s_ beta, q_ 0, h_1] === [q_ 0, h_1] | beta <- [0..m]]
